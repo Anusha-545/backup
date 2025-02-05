8cda62e1ce7c8ea34289f8436028bb1b4b849379
Anusha-545 <kanuparthianusha05@gmail.com>
2025-02-05 11:36:10 +0530

    Add files via upload

---
+++ Anusha/function.py
@@ -0,0 +1,7 @@
+def greet(first_name,last_name):
+    print(f"hello {first_name} {last_name}")
+    print("welcome")
+
+
+greet("anusha","kanuparthi")
+greet("tharun","u")
---
+++ Anusha/reverse_triangle.py
@@ -0,0 +1,11 @@
+# input = 5
+# output reverse traingle
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(' ',end='')
+    for j in range(i,n-1):
+        print('*',end='')
+    for j in range(i,n):
+        print('*',end='')
+    print()
